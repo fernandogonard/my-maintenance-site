a21b313a1378f779af027449f11b7f3a8642ddc4
fernandogonard <matiasgonard1@gmail.com>
2024-07-13 21:33:11 -0300

    ultimos retoques

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './AboutUs.css';
+import teamImage from '../assets/2d1n0wni.bmp'; // Importa la imagen
 
 const AboutUs = () => {
   return (
     <section id="about-us">
       <div className="about-us-container">
         <div className="about-us-text">
-          
           <h2>Quiénes Somos</h2>
           <p>
             En JP Mantenimiento, nos dedicamos a ofrecer servicios integrales de mantenimiento tanto para hogares como para hoteles. Nuestro equipo está compuesto por profesionales altamente capacitados en diversas áreas como electricidad, fontanería, pintura, albañilería y más. Nos enorgullece brindar soluciones eficientes y de alta calidad que aseguren el óptimo funcionamiento y la estética de sus instalaciones.
@@ -15,7 +15,7 @@ const AboutUs = () => {
           </p>
         </div>
         <div className="about-us-image">
-          <img src="src/assets/2d1n0wni.bmp" alt="Equipo de JP Mantenimiento" />
+          <img src={teamImage} alt="Equipo de JP Mantenimiento" />
         </div>
       </div>
     </section>
