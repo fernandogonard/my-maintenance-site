4bf63952dd4305541d5823dd6a95f3855cda718a
fernandogonard <matiasgonard1@gmail.com>
2024-07-16 15:32:46 -0300

    Revert "arreglamos el Seo (srr y etiqutas meta 2.2 )"
    This reverts commit cce3b8ab8a53b3402ec34b6727fd7118d7f83da0.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: true, // Habilitar la generación de mapas de fuentes
     ssr: true,
     outDir: 'dist', // Asegúrate de que la salida se dirija a la carpeta correcta
     rollupOptions: {
-      input: 'src/entry-server.jsx', // Ruta correcta al archivo de entrada del servidor
-      sourcemap: true, // Habilitar la generación de mapas de fuentes
+      input: 'src/entry-server.jsx' // Ruta correcta al archivo de entrada del servidor
     }
   }
 });
