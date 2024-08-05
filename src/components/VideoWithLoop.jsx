4d48fe6c21a087bf662546352b31996c1c723a0d
fernandogonard <matiasgonard1@gmail.com>
2024-08-05 10:32:29 -0300

    cambiamos carrucell  x video  promo whatsapp reparamos fallo2

--- src/components/VideoWithLoop.jsx
+++ src/components/VideoWithLoop.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import './VideoWithLoop.css'; // Puedes reutilizar el archivo CSS existente
+import './VideoWithLoop.css';
 
 const VideoWithLoop = () => {
   return (
     <div className="image-with-carousel">
       <div className="video-container">
         <video autoPlay loop muted>
-        <source src="src/assets/promo.mp4" type="video/mp4" />
-
+          <source src="/assets/promo.mp4" type="video/mp4" />
           Tu navegador no admite el elemento de video.
         </video>
       </div>
