7af9b85f713f8d503da1a218403ae66fe1094d48
fernandogonard <matiasgonard1@gmail.com>
2024-10-09 11:16:37 -0300

    mejoramos el seo y algunos errores en el css

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,11 +1,19 @@
 // src/components/PageNotFound.jsx
 import React from 'react';
+import NotFoundImage from '../assets/404.png'; // Ajusta la ruta según tu estructura de carpetas
+import '../styles/PageNotFound.css'; // Importa el CSS
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className="page-not-found">
+      <div className="background-effect"></div> {/* Fondo animado */}
       <h1>404 - Página No Encontrada</h1>
       <p>Lo sentimos, la página que buscas no existe.</p>
+      <img 
+        src={NotFoundImage} 
+        alt="Página No Encontrada" 
+        style={{ borderRadius: '10px', boxShadow: '0 4px 20px rgba(0, 0, 0, 0.2)' }} // Opciones de estilo adicionales
+      />
     </div>
   );
 };
